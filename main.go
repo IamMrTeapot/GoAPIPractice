5119935edf21b1804c1d67cb97af41c08ca7fa6d
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-01-10 12:42:54 +0700

    change base code

--- main.go
+++ main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w, "Hello World!")
-}
-
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	app := fiber.New()
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	app.Listen(":8080")
 }
